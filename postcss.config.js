985b5526b6a982c37f385e084e4ed1a1e63b1909
mulyant20 <mulyan.t20@gmail.com>
2022-06-22 02:29:18 +0700

    build: configure tailwind

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+  i18n: {
+    locales: ['id-ID'],
+    defaultLocale: 'id-ID',
+    localeDetection: true,
+  },
+}
