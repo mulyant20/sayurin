d68c7b2459bdc40014ea5a86155a520fa089c6ea
mulyant20 <mulyan.t20@gmail.com>
2022-06-22 05:57:56 +0700

    fix: login mitra

--- pages/loginMitra.js
+++ pages/loginMitra.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Link from "next/link";
 
-export default function loginMitra() {
+export default function LoginMitra() {
   const [dataLogin, setDataLogin] = useState({
     mail: "",
     password: "",
