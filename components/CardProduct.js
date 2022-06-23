de0b06977e1d6c6ab8939d86116fd1bfe5bcb9cb
mulyant20 <mulyan.t20@gmail.com>
2022-06-24 02:26:41 +0700

    feat: add CardProduct

---
+++ components/CardProduct.js
@@ -0,0 +1,11 @@
+export default function CardProduct() {
+    return(
+        <div className="w-[160px] h-[180px] bg-white/80 hover:bg-white hover:shadow-sm border border-transparent hover:border-primary rounded-[8px] relative grow lg:grow-0 p-2 duration-[150ms] ease-in">
+            <div className="w-full h-24 bg-gray-100 rounded-[5px] border border-gray-200 overflow-hidden">
+
+            </div>
+            <p className="mt-2 text-md text-gray-400">Brokoli</p>
+            <p>Rp. </p>
+        </div>
+    )
+}
