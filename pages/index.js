a9091e994cacafd38599d974ea1c499f8a8bee45
mulyant20 <mulyan.t20@gmail.com>
2022-06-24 02:27:18 +0700

    feat: add CardProduct inside container

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import CardProduct from '../components/CardProduct'
 import Navbar from '../components/Navbar'
 
 export default function Home() {
@@ -10,6 +11,20 @@ export default function Home() {
         <link rel="icon" href="/logo.svg" />
       </Head>
       <Navbar />
+      <div className='mt-6 max-w-[1100px] h-40 mx-auto px-6 lg:px-0 flex flex-wrap justify-between gap-4 gap-y-4'>
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+        <CardProduct />
+      </div>
     </div>
   )
 }
