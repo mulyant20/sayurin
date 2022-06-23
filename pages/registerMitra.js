6a82ffdc9c44edb6bd9dd72e62756c88fe6b16b3
mulyant20 <mulyan.t20@gmail.com>
2022-06-23 10:42:55 +0700

    feat: update handleSubmit

--- pages/registerMitra.js
+++ pages/registerMitra.js
@@ -1,25 +1,61 @@
+import { useUserAuth } from "../context/UserAuthContextProvider";
+import { auth, createUser } from "../services/firebase";
+import { useRouter } from "next/router";
 import { useState } from "react";
 import Link from "next/link";
+import {
+  GoogleAuthProvider,
+  signInWithPopup,
+  updateProfile,
+} from "firebase/auth";
 
 export default function RegisterMitra() {
+  const router = useRouter();
+  const { signUp } = useUserAuth();
+
   const [dataLogin, setDataLogin] = useState({
+    username: "",
     mail: "",
     password: "",
   });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-
     setDataLogin((prev) => ({
       ...prev,
       [name]: value,
     }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(dataLogin);
+    try {
+      const { user } = await signUp(dataLogin.mail, dataLogin.password);
+      await createUser(user, dataLogin.username, "mitra");
+      await updateProfile(user, { displayName: dataLogin.username });
+      setDataLogin({
+        username: "",
+        mail: "",
+        password: "",
+      });
+      router.push("/");
+    } catch (err) {
+      console.log(err);
+    }
   };
+
+  const signInGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((res) => {
+        createUser(res.user, res.user.displayName, "mitra");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    router.push("/");
+  };
+
   return (
     <div className="h-screen w-screen flex justify-between">
       <div className="h-full bg-white w-screen md:w-1/2 flex justify-center items-center">
@@ -27,6 +63,14 @@ export default function RegisterMitra() {
           <h1 className="mb-6 text-2xl text-gray-800">Daftar sebagai mitra</h1>
           <form onSubmit={handleSubmit}>
             <div className="flex flex-col gap-2">
+              <input
+                type="text"
+                placeholder="Name"
+                value={dataLogin.username}
+                onChange={handleChange}
+                name="username"
+                className="border border-gray-200 rounded px-4 py-3 outline-none"
+              />
               <input
                 type="text"
                 placeholder="Email"
@@ -51,7 +95,10 @@ export default function RegisterMitra() {
               </button>
             </div>
           </form>
-          <button className="w-full mt-4 py-2 text-white bg-blue-500 rounded shadow font-semibold">
+          <button
+            className="w-full mt-4 py-2 text-white bg-blue-500 rounded shadow font-semibold"
+            onClick={signInGoogle}
+          >
             Google
           </button>
           <p className="mt-2 text-gray-400">
