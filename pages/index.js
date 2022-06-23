9e3126c439bb41cd2204e5a7492ed78612a721f6
mulyant20 <mulyan.t20@gmail.com>
2022-06-24 01:27:29 +0700

    feat: add navbar

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,15 @@
 import Head from 'next/head'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        
         <title>Sayurin</title>
         <meta name="description" content="Sayurin ecommerce jual beli sayuran dengan jaminan kualitas terbaik" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.svg" />
       </Head>
-
-      
+      <Navbar />
     </div>
   )
 }
