b5212e9305e687b483ddda13df892963261a74d8
mulyant20 <mulyan.t20@gmail.com>
2022-06-24 01:26:44 +0700

    feat: add SearchField component

---
+++ components/SearchField.js
@@ -0,0 +1,10 @@
+export default function SearchField() {
+    return(
+        <div className="relative md:w-full lg:max-w-[480px] h-[38px]">
+            <input className="bg-white w-full h-full rounded outline-none px-4"/>
+            <div>
+
+            </div>
+        </div>
+    )
+}
