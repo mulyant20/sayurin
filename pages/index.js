e870f7fa331e93220e170de3a79f1c900e1b836d
mulyant20 <mulyan.t20@gmail.com>
2022-06-24 15:03:58 +0700

    feat: add addToCart func

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,51 @@
-import Head from 'next/head'
-import CardProduct from '../components/CardProduct'
-import Navbar from '../components/Navbar'
-import { useProductContext } from '../context/ProductContextProvider'
+import Head from "next/head";
+import { useState } from "react";
+import CardProduct from "../components/CardProduct";
+import Navbar from "../components/Navbar";
+import { useProductContext } from "../context/ProductContextProvider";
 
 export default function Home() {
-  const { product } = useProductContext()
-  console.log(product)
+  const { product } = useProductContext();
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (selectedItem) => {
+    let array = [...cart];
+    
+    if (!array.includes(selectedItem)) {
+      array.push(selectedItem);
+      localStorage.setItem("cart", JSON.stringify(array));
+      setCart(array);
+    } else {
+      console.log("Already added");
+    }
+  };
+
   return (
     <div>
       <Head>
         <title>Sayurin</title>
-        <meta name="description" content="Sayurin ecommerce jual beli sayuran dengan jaminan kualitas terbaik" />
+        <meta
+          name="description"
+          content="Sayurin ecommerce jual beli sayuran dengan jaminan kualitas terbaik"
+        />
         <link rel="icon" href="/logo.svg" />
       </Head>
       <Navbar />
-      <div className='mt-6 max-w-[1100px] h-40 mx-auto px-6 lg:px-0 flex flex-wrap justify-between gap-2 lg:gap-4'>
-        {product.map(item => {
-          return  <CardProduct 
-                    key={item.id} 
-                    nama={item.nama} 
-                    harga={item.harga} 
-                    satuan={item.satuan} 
-                    jumbeli={item.jumBeli} 
-                    diskon={item.diskon}
-                  />
+      <div className="mt-6 max-w-[1100px] h-40 mx-auto px-6 lg:px-0 flex flex-wrap justify-between gap-2 lg:gap-4">
+        {product.map((item) => {
+          return (
+            <CardProduct
+              key={item.id}
+              nama={item.nama}
+              harga={item.harga}
+              satuan={item.satuan}
+              jumbeli={item.jumBeli}
+              diskon={item.diskon}
+              click={() => addToCart(item.id)}
+            />
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
