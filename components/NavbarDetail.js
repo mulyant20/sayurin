a7bd32b6808d2460ed2d83580535801855d88fd9
mulyant20 <mulyan.t20@gmail.com>
2022-06-25 02:12:24 +0700

    feat: add NavbarDetail component

---
+++ components/NavbarDetail.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+export default function NavbarDetail({title, path}) {
+    return (
+        <div className="w-screen h-72px bg-primary">
+            <div className="max-w-[500px] h-full h-24 flex items-center justify-center relative mx-auto">
+            <Link href={path}>
+                <button className="absolute top-1/2 -translate-y-1/2 left-0 w-[32px] h-[32px] flex justify-center items-center hover:bg-white/20 rounded duration-[160ms] ease-in">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFF" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                    </svg>
+                </button>
+            </Link>
+            <h1 className="text-[20px] font-semibold text-white">
+                {title}
+            </h1>
+        </div>
+        </div>
+    )
+}
