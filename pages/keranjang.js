5501508aa2d1835ad3275b5139e40b11cb821bc0
mulyant20 <mulyan.t20@gmail.com>
2022-06-25 02:12:05 +0700

    feat: add deleteAllCart func

--- pages/keranjang.js
+++ pages/keranjang.js
@@ -1,24 +1,30 @@
-import { async } from "@firebase/util";
-import Head from "next/head";
+import NavbarDetail from "../components/NavbarDetail";
+import CartProduct from "../components/CartProduct";
 import { useEffect, useState } from "react";
-import CardProduct from "../components/CardProduct";
-import Navbar from "../components/Navbar";
-import { useProductContext } from "../context/ProductContextProvider";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Button from "../components/Button";
 
 export default function Keranjang() {
   const [productCart, setProductCart] = useState([]);
   const localCart = [];
+  const router = useRouter();
 
   const checkStorage = (key) => {
     const local = localStorage.getItem(key);
     localCart = JSON.parse(local);
   };
 
+  const deleteAllCart = () => {
+    localStorage.removeItem("cart");
+    router.reload(window.location.pathname);
+  };
+
   useEffect(() => {
     checkStorage("cart");
     if (localCart) setProductCart(localCart);
   }, []);
-  
+
   return (
     <>
       <Head>
@@ -29,21 +35,33 @@ export default function Keranjang() {
         />
         <link rel="icon" href="/logo.svg" />
       </Head>
-      <Navbar />
-      <div className="mt-6 max-w-[1100px] h-40 mx-auto px-6 lg:px-0 flex flex-wrap justify-between gap-2 lg:gap-4">
-        {productCart.map((item,i) => {
-            return (
-                <CardProduct
+      <NavbarDetail path="/" title="Keranjang" />
+      <div className="mt-6 max-w-[500px] h-fit mx-auto px-6 lg:px-0 flex flex-col justify-between gap-[24px] lg:gap-[32px]">
+        {productCart != '' ? (
+          <>
+            <div className="flex justify-end">
+              <button className="text-red-500 text-sm" onClick={deleteAllCart}>
+                Hapus daftar
+              </button>
+            </div>
+            {productCart.map((item, i) => {
+              return (
+                <CartProduct
                   key={productCart[i][0].id}
                   nama={productCart[i][0].nama}
                   harga={productCart[i][0].harga}
                   satuan={productCart[i][0].satuan}
-                  jumbeli={productCart[i][0].jumBeli}
                   diskon={productCart[i][0].diskon}
-                  click={() => addToCart(productCart[i][0].id)}
                 />
               );
-        })}
+            })}
+            <div className="w-full h-fit flex justify-end">
+              <Button goto path='/hehe' text='Checkout' />
+            </div>
+          </>
+        ) : (
+          <div className="w-full h-40 flex items-center justify-center">tidak ada</div>
+        )}
       </div>
     </>
   );
