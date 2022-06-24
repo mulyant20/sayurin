afb0a2a9fc2f746dea27baeae5070cbcd87d241f
mulyant20 <mulyan.t20@gmail.com>
2022-06-25 02:27:07 +0700

    style: change height

--- components/NavbarDetail.js
+++ components/NavbarDetail.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function NavbarDetail({title, path}) {
     return (
-        <div className="w-screen h-72px bg-primary">
-            <div className="max-w-[500px] h-full h-24 flex items-center justify-center relative mx-auto">
+        <div className="w-screen h-[56px] lg:h-[72px] bg-primary">
+            <div className="max-w-[500px] h-full flex items-center justify-center relative mx-auto">
             <Link href={path}>
                 <button className="absolute top-1/2 -translate-y-1/2 left-0 w-[32px] h-[32px] flex justify-center items-center hover:bg-white/20 rounded duration-[160ms] ease-in">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#FFF" viewBox="0 0 16 16">
