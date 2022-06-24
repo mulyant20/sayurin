b972324bd0c9f1f2c031c009bbae7300a0646c1e
mulyant20 <mulyan.t20@gmail.com>
2022-06-25 02:13:39 +0700

    feat: add new page

---
+++ pages/hehe.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Hehe() {
+  return (
+    <div>
+      <div className="w-screen h-screen p-6">
+        <Image src="https://i.imgur.com/FKmX7dt.gif" layout="fill" />
+      </div>
+    </div>
+  );
+}
