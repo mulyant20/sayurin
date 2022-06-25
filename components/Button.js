c6ef38a037997ddff02aa4b31b80b3f58106da18
mulyant20 <mulyan.t20@gmail.com>
2022-06-25 23:00:21 +0700

    style: change size button

--- components/Button.js
+++ components/Button.js
@@ -8,7 +8,7 @@ export default function Button({ goto, path, text, click }) {
   };
   return (
     <button
-      className="w-[200px] lg:w-[120px] h-[56px] lg:h-[40px] btn bg-primary rounded-[16px] lg:rounded-[10px] duration-[150ms] ease-in"
+      className="min-w-[120px] h-[48px] px-6 btn bg-primary rounded-[10px] duration-[150ms] ease-in"
       onClick={goto ? moveTo : click}
     >
       <span className="text-white text-[18px] lg:text-[15px] font-semibold">{text}</span>
