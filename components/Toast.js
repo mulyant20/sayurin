757a98772241e40f651899915c2575877fbc7b62
mulyant20 <mulyan.t20@gmail.com>
2022-06-26 12:13:19 +0700

    style: add animation at mobile

--- components/Toast.js
+++ components/Toast.js
@@ -1,6 +1,6 @@
 export default function Toast({ text }) {
   return (
-    <div className="max-w-[400px] h-fit fixed bottom-6 right-6 lg:bottom-3 lg:right-3 toast bg-slate-600 text-white rounded px-8 py-3 text-center">
+    <div className="max-w-[400px] h-fit fixed top-6 right-6 lg:bottom-3 lg:right-3 toast bg-slate-600 text-white rounded px-8 py-3 text-center">
       {text}
     </div>
   );
